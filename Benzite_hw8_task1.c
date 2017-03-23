d59fcaed6f646dafb46c35796d57a7ad670a38fe
kyle webb <kylewebb4@weber.edu>
2017-03-23 16:22:06 +0000

    File name

---
+++ Benzite_hw8_task1.c
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Benzite_hw8_task1.c
+ *       	Usage:  ./Benzite_hw8_task1.c
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  03/23/2017 04:21:25 PM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Kyle Webb (), kylewebbear@weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>		/* Standard Library */
+
+/* Function Prototypes */
+
+/* Main Program */
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
